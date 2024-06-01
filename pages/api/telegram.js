712324f72b78706b486e59074ec572829bce200f
swizz <emailswizz@gmail.com>
2024-06-01 05:55:12 -0400

    ok

--- pages/api/telegram.js
+++ pages/api/telegram.js
@@ -6,8 +6,13 @@ export default function handler(req, res) {
 
   if (req.method === 'POST') {
     const { body } = req;
-    bot.processUpdate(body);
-    res.status(200).send('OK');
+    try {
+      bot.processUpdate(body);
+      res.status(200).send('OK');
+    } catch (error) {
+      console.error('Error processing update:', error);
+      res.status(500).send('Internal Server Error');
+    }
   } else {
     console.log('Method not allowed:', req.method);
     res.status(405).send('Method Not Allowed');
