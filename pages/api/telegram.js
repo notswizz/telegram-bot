ed78aed05cdeab9e0638e229077be945b3ef3145
swizz <emailswizz@gmail.com>
2024-06-01 05:50:09 -0400

    ok

--- pages/api/telegram.js
+++ pages/api/telegram.js
@@ -4,9 +4,11 @@ const bot = require('../../lib/telegramBot');
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const { body } = req;
+    console.log('Incoming request:', req.method, req.body);
     bot.processUpdate(body);
     res.status(200).send('OK');
   } else {
+    console.log('Method not allowed:', req.method);
     res.status(405).send('Method Not Allowed');
   }
 }
