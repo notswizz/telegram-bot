344ab352ccd4b1935a344433a82ce96d3963ce2a
swizz <emailswizz@gmail.com>
2024-06-01 05:41:41 -0400

    ok

--- scripts/setWebhook.js
+++ scripts/setWebhook.js
@@ -1,7 +1,9 @@
 // scripts/setWebhook.js
 const dotenv = require('dotenv');
+const path = require('path');
 
-dotenv.config();
+// Load environment variables from .env.local
+dotenv.config({ path: path.resolve(__dirname, '../.env.local') });
 
 const { TELEGRAM_BOT_TOKEN } = process.env;
 
@@ -9,6 +11,8 @@ if (!TELEGRAM_BOT_TOKEN) {
   console.error('TELEGRAM_BOT_TOKEN is not defined in .env.local');
   process.exit(1);
 }
+
+console.log('TELEGRAM_BOT_TOKEN:', TELEGRAM_BOT_TOKEN);
 
 const webhookUrl = 'https://notswizzbot.vercel.app/api/telegram'; // Use your actual deployed URL
 
