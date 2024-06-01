807f40a1f49f95bdd21471cec4aece03506d0f6a
swizz <emailswizz@gmail.com>
2024-06-01 05:27:12 -0400

    ok

--- scripts/setWebhook.js
+++ scripts/setWebhook.js
@@ -1,13 +1,12 @@
 // scripts/setWebhook.js
-const fetch = require('node-fetch');
 const { TELEGRAM_BOT_TOKEN } = process.env;
 const webhookUrl = 'https://notswizzbot.vercel.app/api/telegram';
 
 const setWebhook = async () => {
+  const fetch = await import('node-fetch').then(module => module.default);
   const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/setWebhook?url=${webhookUrl}`);
   const data = await response.json();
   console.log(data);
 };
 
 setWebhook();
-0
