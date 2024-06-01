04f1e2f06b8d7cde7089b636397f694976cbee04
swizz <emailswizz@gmail.com>
2024-06-01 05:34:46 -0400

    ok

--- scripts/setWebhook.js
+++ scripts/setWebhook.js
@@ -1,6 +1,5 @@
 // scripts/setWebhook.js
-import fetch from 'node-fetch';
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 
 dotenv.config();
 
@@ -8,6 +7,7 @@ const { TELEGRAM_BOT_TOKEN } = process.env;
 const webhookUrl = 'https://notswizzbot.vercel.app/api/telegram'; // Replace with your actual deployed URL
 
 const setWebhook = async () => {
+  const fetch = await import('node-fetch').then(module => module.default);
   const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/setWebhook?url=${webhookUrl}`);
   const data = await response.json();
   console.log(data);
